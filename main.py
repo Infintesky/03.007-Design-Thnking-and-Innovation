a4e9f1ab2255a39f03b2b5c7d50f4c4fd55c8aa1
Infintesky <weiyang2912@hotmail.com>
2025-04-11 11:07:56 +0800

    added working lir

--- main.py
+++ main.py
@@ -1,25 +1,57 @@
-import cv2 as cv
+import largestinteriorrectangle as lir
+import cv2
+from PIL import Image
 import numpy as np
-import matplotlib.pyplot as plt
 
-# Load the image in grayscale
-image_path = "img.jpg"
-img = cv.imread(image_path, cv.IMREAD_GRAYSCALE)
+# Capture image from webcam and save
+# ret, frame = cv2.VideoCapture(0).read()
+# cv2.imwrite("img2.png", frame)
 
-# Apply Otsu's thresholding to classify dark and light regions
-_, binary = cv.threshold(img, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)
+img = Image.open("./tests/test3.png")
+img.save("output.jpg", quality=5)  # For JPEG
 
-canny_e = cv.Canny(binary, 10, 1000)
+image = cv2.imread('output.jpg')
 
-# Display results
-plt.figure(figsize=(12, 4))
-plt.subplot(1, 3, 1), plt.imshow(img, cmap='gray')
-plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-plt.subplot(1, 3, 2), plt.imshow(binary, cmap='gray')
-plt.title('Thresholded Image'), plt.xticks([]), plt.yticks([])
+# Binarize the image (convert to binary format)
+_, binary_image = cv2.threshold(gray_image, 127, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
 
-plt.subplot(1, 3, 3), plt.imshow(canny_e, cmap='gray')
-plt.title('Canny Filter'), plt.xticks([]), plt.yticks([])
+# Clean up grain
+kernel = np.ones((60,60),np.uint8)
+binary_image = cv2.morphologyEx(binary_image, cv2.MORPH_CLOSE, kernel)
 
-plt.show()
+# Find contours
+contours, _ = cv2.findContours(binary_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+
+# Extract the first contour
+print("number of contours:", len(contours))
+c = max(contours, key = cv2.contourArea)
+contour = c
+
+# Convert contour to NumPy array
+contour_array = contour[:, 0, :]
+
+# Convert the binary image to a boolean array
+grid = (binary_image == 255)
+
+# Call largestinteriorrectangle function
+largest_rectangle = lir.lir(grid, contour_array)
+
+image_with_contours = image.copy()
+image_with_contours = cv2.drawContours(image_with_contours, contour, -1, (0, 255, 0), 3)
+cv2.rectangle(image_with_contours, lir.pt1(largest_rectangle), lir.pt2(largest_rectangle), (255, 0, 0), 2)
+
+top_left = lir.pt1(largest_rectangle)
+bottom_right = lir.pt2(largest_rectangle)
+
+width = bottom_right[0] - top_left[0] + 1
+height = bottom_right[1] - top_left[1] + 1
+
+area = width * height
+print(area)  
+
+cv2.imshow("",image_with_contours)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+

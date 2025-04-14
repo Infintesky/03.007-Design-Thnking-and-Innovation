7c4ab01706f923b70c3957d41744f7e9712b49d6
Infintesky <weiyang2912@hotmail.com>
2025-04-14 12:38:14 +0800

    added obj detection model

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 import largestinteriorrectangle as lir
 import cv2
-from PIL import Image
 import numpy as np
 
 # Capture image from webcam and save
 # ret, frame = cv2.VideoCapture(0).read()
 # cv2.imwrite("img2.png", frame)
 
-img = Image.open("./tests/test3.png")
-img.save("output.jpg", quality=5)  # For JPEG
-
-image = cv2.imread('output.jpg')
+raw_image = cv2.imread('./tests/test3.png')
+image = cv2.resize(raw_image, None, fx = 0.4, fy = 0.4)
 
 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
@@ -25,7 +22,7 @@ binary_image = cv2.morphologyEx(binary_image, cv2.MORPH_CLOSE, kernel)
 contours, _ = cv2.findContours(binary_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
 # Extract the first contour
-print("number of contours:", len(contours))
+print("[+] Number of contours detected:", len(contours))
 c = max(contours, key = cv2.contourArea)
 contour = c
 
@@ -38,18 +35,23 @@ grid = (binary_image == 255)
 # Call largestinteriorrectangle function
 largest_rectangle = lir.lir(grid, contour_array)
 
-image_with_contours = image.copy()
-image_with_contours = cv2.drawContours(image_with_contours, contour, -1, (0, 255, 0), 3)
-cv2.rectangle(image_with_contours, lir.pt1(largest_rectangle), lir.pt2(largest_rectangle), (255, 0, 0), 2)
-
 top_left = lir.pt1(largest_rectangle)
 bottom_right = lir.pt2(largest_rectangle)
 
+image_with_contours = image.copy()
+image_with_contours = cv2.drawContours(image_with_contours, contour, -1, (0, 255, 0), 3)
+cv2.rectangle(image_with_contours, top_left, bottom_right, (255, 0, 0), 2)
+
+# Calculate area of pixels
 width = bottom_right[0] - top_left[0] + 1
 height = bottom_right[1] - top_left[1] + 1
 
 area = width * height
-print(area)  
+
+print("[+] Width in pixels:", width)
+print("[+] Height in pixels:", height) 
+print("[+] Area of rectangle:", area) 
+print("[+] Area of contour:", cv2.contourArea(c))
 
 cv2.imshow("",image_with_contours)
 cv2.waitKey(0)
